73f9e84d4d2181f5837dc42bc313e17d85c3a45c
mattminder <myfiles@Mattus-MacBook-Pro.local>
2018-10-12 12:04:28 +0200

    Created base structure for program

---
+++ src/main.py
@@ -0,0 +1,10 @@
+"""Main file for executing the entire project"""
+
+# Header
+from helpers.proj1_helpers import *
+
+
+# Body
+# Data loading
+test = load_csv_data("../all/test.csv")
+train = load_csv_data("../all/train.csv")
